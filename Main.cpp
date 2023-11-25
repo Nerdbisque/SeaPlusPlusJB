ccdecd5f2c13c25f12ce2d75d05cc8947b182fa6
Nerdbisque <113635080+Nerdbisque@users.noreply.github.com>
2023-11-25 16:40:15 -0700

    j

---
+++ Main.cpp
@@ -0,0 +1,5 @@
+//
+// Created by bisqu on 11/25/2023.
+//
+
+#include "Main.h"
---
+++ Main.h
@@ -0,0 +1,14 @@
+//
+// Created by bisqu on 11/25/2023.
+//
+
+#ifndef SEAPLUSPLUS_MAIN_H
+#define SEAPLUSPLUS_MAIN_H
+
+
+class Main {
+
+};
+
+
+#endif //SEAPLUSPLUS_MAIN_H
